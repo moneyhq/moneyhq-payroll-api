4b9393a8e9203a395e5f2bb8a0ca509779c124bf
Temi Adenuga <a.adenuga@hotmail.com>
2024-07-02 19:44:05 +0100

    Add query to get employees data from DB

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import "dotenv/config.js";
 import cors from "cors";
+import employeesRoute from "./routes/employeesRoutes.js";
+
+const PORT = process.env.PORT;
+const BACKEND_URL = process.env.BACKEND_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const app = express();
 app.use(express.json());
 
 app.use(cors({ origin: FRONTEND_URL }));
+
+app.use("/api/employees", employeesRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${BACKEND_URL}:${PORT}`);
